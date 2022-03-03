046c42a8bc493f4867f3dd4038fb0977ddcf9291
MChambersIV <chambersm025@gmail.com>
2022-03-02 16:51:06 -0800

    moved page components into pages directory, added bootstrap for styling

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a>
+          Home
+        </a>
+      </li>
+      </ul>
+    )
+}
+
+export default Nav;
