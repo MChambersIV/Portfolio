283e1a7a9c20570e70f336758a171aac10d03621
MChambersIV <chambersm025@gmail.com>
2023-01-23 08:47:54 -0800

    Changed background and text color for Header as well.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
     return (
-        <h1 className="title bg-dark text-light row-start">Chambers' Portfolio</h1>
+        <h1 className="title bg-light text-dark row-start">Chambers' Portfolio</h1>
     )
 }
 
