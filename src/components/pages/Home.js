1f3fcb418c94ff1104717c555e4d3557666fe380
MChambersIV <chambersm025@gmail.com>
2023-03-05 05:19:19 -0800

    centered some text elements in Header.js and Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Self from '../images/self.jpg'
 
 function Home() {
     return (
 
     <div>
-        <p class="text-justify"> Welcome! </p>
+        <p class="text-center"> Welcome! </p>
     </div>
     )
 }
