0c4a64fc603bc5a3a297e85303ddfd88c3d8a7c1
MChambersIV <chambersm025@gmail.com>
2023-03-19 02:39:14 -0700

    changed image sizing elements in About.js further

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,8 +4,8 @@ import Self from '../images/self.jpg'
 function About() {
     return (
     <div>   
-        <img  style={{ width: "15%", height: "15%" }} src={Self} alt="Portrait of Myself"/> 
-        <p>My name is Millard Chambers and I'm an apsiring Software Developer.</p>
+        <img  style={{  width: "240px", height: "320px" }} src={Self} alt="Portrait of Myself"/> 
+        <p>My name is Millard Chambers and I'm a Software Developer.</p>
         <p>I grew up all around the San Francisco Bay Area, exposing me to cultures and ways of life as varied as the people that people that make the Bay Area what it is.</p>
         <p>Of course the Bay Area isn't just a bountiful cultural center, or perhaps because it is so, it's home to the foremost high technology hub of the world.</p>
         <p>Watching the rapid pace of technology's advancement and how it improve the lives of people the world over because of talented and dedicated individuals inspired me journey down this path help leave it better than I found it.</p>
