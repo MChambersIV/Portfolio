fec5949c7625b316b47794a4141212cf9096105f
MChambersIV <chambersm025@gmail.com>
2023-03-23 10:12:51 -0700

    replaced bootstrap styling syntax in className tag with new object containing styling parameters

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,36 @@
 import React from 'react';
 
+const boxStyle = {
+  color: "orange",
+  borderStyle: "solid",
+  borderRadius: "5px",
+  borderWidth: "1px",
+  borderColor: "orange",
+  margin: "10px"
+
+};
+
 
 export default function Nav({ changePage }) {
   return (
     <ul className="nav justify-content-center mt-3">
       <li className="nav-item">
-        <a href="#home" onClick= { () => changePage('Home') } className="nav-link text-warning border border-warning mx-1 rounded" >
+        <a href="#home" onClick= { () => changePage('Home') } className="nav-link" style={boxStyle} >
           Home
         </a>
       </li>
       <li className="nav-item">
-        <a href="#about" onClick= { () => changePage('About') }className="nav-link text-warning border border-warning mx-1 rounded">
+        <a href="#about" onClick= { () => changePage('About') }className="nav-link" style={boxStyle}>
           About
         </a>
       </li>
       <li className="nav-item">
-        <a href="#projects" onClick= { () => changePage('Projects') } className="nav-link text-warning border border-warning mx-1 rounded">
+        <a href="#projects" onClick= { () => changePage('Projects') } className="nav-link"  style={boxStyle}>
           Projects
         </a>
       </li>
       <li className="nav-item">
-        <a href="#resume" onClick= { () => changePage('Resume') } className="nav-link text-warning border border-warning mx-1 rounded">
+        <a href="#resume" onClick= { () => changePage('Resume') } className="nav-link" style={boxStyle}>
           Resume
         </a>
       </li>
