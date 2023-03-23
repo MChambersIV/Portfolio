b2ef4e67ca94268bc1d6f3f275eb55d0bf9df211
MChambersIV <chambersm025@gmail.com>
2023-03-23 10:01:55 -0700

    added object with styling paramters to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
 import React from "react";
 
+const headStyle = {
+    background: "black",
+    color: "white",
+    fontSize: "50px",
+    fontFamily: "Roboto"
+};
+
 function Header() {
     return (
-        <h1 className="title text-center row-start">Chambers</h1>
+        <h1 className="title text-center row-start" style={headStyle}>Chambers</h1>
     )
 }
 
