a870eaf41da325386ff7bb2d8e27ca9522f509e4
MChambersIV <chambersm025@gmail.com>
2023-03-22 23:19:22 -0700

    Updated styling elements in Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,24 +3,24 @@ import React from 'react';
 
 export default function Nav({ changePage }) {
   return (
-    <ul className="nav justify-content-center">
+    <ul className="nav justify-content-center mr-1">
       <li className="nav-item">
-        <a href="#home" onClick= { () => changePage('Home') } className="nav-link" >
+        <a href="#home" onClick= { () => changePage('Home') } className="nav-link text-warning border border-warning mx-1 rounded" >
           Home
         </a>
       </li>
       <li className="nav-item">
-        <a href="#about" onClick= { () => changePage('About') }className="nav-link">
+        <a href="#about" onClick= { () => changePage('About') }className="nav-link text-warning border border-warning mx-1 rounded">
           About
         </a>
       </li>
       <li className="nav-item">
-        <a href="#projects" onClick= { () => changePage('Projects') } className="nav-link">
+        <a href="#projects" onClick= { () => changePage('Projects') } className="nav-link text-warning border border-warning mx-1 rounded">
           Projects
         </a>
       </li>
       <li className="nav-item">
-        <a href="#resume" onClick= { () => changePage('Resume') } className="nav-link">
+        <a href="#resume" onClick= { () => changePage('Resume') } className="nav-link text-warning border border-warning mx-1 rounded">
           Resume
         </a>
       </li>
