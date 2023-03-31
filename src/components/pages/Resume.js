cc7d003602432ef44068e982c52c337f937eb813
MChambersIV <chambersm025@gmail.com>
2023-03-31 14:42:15 -0700

    resized Header links in Header.js, styling addtions in Resume.js

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,15 +1,16 @@
 import React from "react";
 
 const boxStyle = {
-    display: "block",
-    color: "red",
+    textDecoration: "none",
+    color: "green",
+    padding: "5px",
+    fontSize: "18px",
     borderStyle: "solid",
     borderRadius: "1px",
     borderWidth: "2px",
-    borderColor: "red",
-    margin: "1px",
-    justifyContent: "center"
-
+    borderColor: "green",
+    marginTop: "15px",
+    marginBottom: "15px"
 };
 
 const listStyle = {
@@ -17,21 +18,38 @@ const listStyle = {
     marginLeft: "auto",
     marginRight: "auto",
 
-}
+};
+
+const titleStyle = {
+    display: "block",
+    fontFamily: "Roboto",
+    marginleft: "auto",
+    marginRight: "auto"
+
+};
 
 function Resume() {
     return (
         <div>
-         
-         <a style={boxStyle} href="https://drive.google.com/file/d/1zWUbt27l-IwGQGAYzvlKaHQCopJl1Und/view?usp=sharing">Resume</a>
-         
+         <div class="nav justify-content-center">
+         <a class="nav-item" style={boxStyle} href="https://drive.google.com/file/d/1zWUbt27l-IwGQGAYzvlKaHQCopJl1Und/view?usp=sharing">Resume</a>
+        </div>
          <div class="d-flex flex-column">
+            <h3 style={titleStyle}>Languages:</h3>
             <p style={listStyle}>JavaScript</p>
             <p style={listStyle}>React</p>
-            <p style={listStyle}>SQL</p>
-            <p style={listStyle}>MongoDB</p>
-            <p style={listStyle}>CSS</p>
+            <p style={listStyle}>CSS3</p>
             <p style={listStyle}>HTML</p>
+            
+            <h3 style={titleStyle}>Technologies:</h3>
+            <p style={listStyle}>Git</p>
+            <p style={listStyle}>Node</p>
+            <p style={listStyle}>BootStrap</p>
+            <p style={listStyle}>mySQL</p>
+            <p style={listStyle}>noSQL</p>
+            <p style={listStyle}>MongoDB</p>
+            
+            
          </div>
         
         </div>
